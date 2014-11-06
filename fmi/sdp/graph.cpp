246e732ae8cd945a1603f4a34e5cac5dd5e00a37
Veselin Dobrev <baiumbg@gmail.com>
2014-11-06 16:29:26 +0200

    Refactored code

--- fmi/sdp/graph.cpp
+++ fmi/sdp/graph.cpp
@@ -15,36 +15,36 @@ struct Edge {
 
 class Graph {
 private:
-  map<int, vector<int> > vertices;
+  map<int, vector<int> > graph;
 public:
   Graph(Edge* edges, int m) {
     for(int i = 0; i < m; i ++) {
-      vertices[edges[i].start].push_back(edges[i].end);
+      graph[edges[i].start].push_back(edges[i].end);
     }
   }
   void insertEdge(Edge e) {
-    vertices[e.start].push_back(e.end);
+    graph[e.start].push_back(e.end);
   }
   void insertNode(int node) {
-    vertices[node] = vector<int>();
+    graph[node] = vector<int>();
   }
   void removeEdge(Edge e) {
-    for(int i = 0; i < vertices[e.start].size(); i++) {
-      if(vertices[e.start].at(i) == e.end)
-        vertices[e.start].erase(vertices[e.start].begin() + i);
+    for(int i = 0; i < graph[e.start].size(); i++) {
+      if(graph[e.start].at(i) == e.end)
+        graph[e.start].erase(graph[e.start].begin() + i);
     }
   }
   void removeNode(int node) {
-    for(map<int, vector<int> >::iterator i = vertices.begin(); i != vertices.end(); i++) {
+    for(map<int, vector<int> >::iterator i = graph.begin(); i != graph.end(); i++) {
       for(int j = 0; j < i->second.size(); j++) {
         if(i -> second.at(j) == node) i -> second.erase(i -> second.begin() + j);
       }
     }
-    map<int, vector<int> >::iterator it = vertices.find(node);
-    vertices.erase(it);
+    map<int, vector<int> >::iterator it = graph.find(node);
+    graph.erase(it);
   }
   void print() {
-    for(map<int, vector<int> >::iterator i = vertices.begin();i!=vertices.end();i++) {
+    for(map<int, vector<int> >::iterator i = graph.begin();i!=graph.end();i++) {
       cout << i -> first << " -> ";
       for(int j = 0; j < i -> second.size(); j++) {
         cout << i -> second.at(j) << ", ";
